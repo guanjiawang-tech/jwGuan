3afa814b4db22ca23cc8a01437731af2f5825c78
guanjiawang-tech <guanjiawang1@gmail.com>
2025-09-23 21:23:34 +0800

    初始化名片完成

--- config/lang.js
+++ config/lang.js
@@ -3,12 +3,18 @@ const langConfig = {
     username: "关佳旺",
     language: "语言",
     english: "英文",
-    chinese: "中文"
+    chinese: "中文",
+    university: "沈阳工学院",
+	profession: "计算机科学与技术",
+	technologyStack: "技术栈"
   },
   en: {
     username: "Guan Jiawang",
     language: "Language",
     english: "EN",
-    chinese: "CN"
+    chinese: "CN",
+    university: "ShenYang Institute Of Technology",
+	profession: "Computer Science and Technology",
+	technologyStack: "technology stack"
   }
 };
